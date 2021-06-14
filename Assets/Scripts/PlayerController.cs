6d654316a7b080f1cc373d4cd42be79df21e4fd9
Ian Kirk <iankirk@live.com>
2021-06-14 18:58:19 +1000

    HP and Energy Implementation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rigid2D;
     public float acceleration;
@@ -10,18 +10,29 @@ public class PlayerMovement : MonoBehaviour
     public float speedThreshold;
     public float torqueThreshold;
 
+    public float maxHP;
+    public float maxEnergy;
+    public float HP;
+    public float energy;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rigid2D = gameObject.GetComponent<Rigidbody2D>();
+        HP = maxHP;
+        energy = maxEnergy;
     }
 
     // Update is called once per frame
     void Update()
     {
-        rigid2D.AddForce(acceleration * Time.deltaTime * transform.up * Input.GetAxis("Vertical"));
-        rigid2D.AddTorque(handling * Time.deltaTime * -Input.GetAxis("Horizontal"));
+        if (energy > 0)
+        {
+            rigid2D.AddForce(acceleration * Time.deltaTime * transform.up * Input.GetAxis("Vertical"));
+            rigid2D.AddTorque(handling * Time.deltaTime * -Input.GetAxis("Horizontal"));
+            energy -= (acceleration * Mathf.Abs(Input.GetAxis("Vertical")) + handling * Mathf.Abs(Input.GetAxis("Horizontal")) * 0.1f) * Time.deltaTime;
+        }
         if (rigid2D.velocity.magnitude > speedThreshold)
         {
             rigid2D.drag = Mathf.Pow(2, (rigid2D.velocity.magnitude - speedThreshold) * 0.01f + 1);
