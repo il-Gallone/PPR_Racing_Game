7acb1b67c3ae94f2a73d3bd178e62dae7a2f9882
Ian Kirk <iankirk@live.com>
2021-06-15 17:56:53 +1000

    Objective Radar Changes.
    Added a radar ping that shows the direction of an objective that is out of radar range

--- Assets/Scripts/RadarPing.cs
+++ Assets/Scripts/RadarPing.cs
@@ -6,21 +6,40 @@ public class RadarPing : MonoBehaviour
 {
     SpriteRenderer renderer;
     float pulse = 0;
+    public bool directed;
+    public Transform radarObject;
+    public Transform targetCamera;
 
     // Start is called before the first frame update
     void Start()
     {
+        radarObject = transform.parent;
+        targetCamera = Camera.main.transform;
         renderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        pulse += Time.deltaTime;
-        if(pulse >=1)
+        if (!directed || (directed && Vector2.Distance(radarObject.position, targetCamera.position) >= 9.5f))
         {
-            pulse--;
-            renderer.enabled = !renderer.enabled;
+            pulse += Time.deltaTime;
+            if (pulse >= 1)
+            {
+                pulse--;
+                renderer.enabled = !renderer.enabled; 
+            }
+        }
+        else
+        {
+            renderer.enabled = false;
+        }
+        if (directed)
+        {
+            Vector2 direction = (radarObject.position - targetCamera.position);
+            transform.position = direction * 9.5f / direction.magnitude;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90;
+            transform.eulerAngles = new Vector3(0, 0, angle);
         }
     }
 }
