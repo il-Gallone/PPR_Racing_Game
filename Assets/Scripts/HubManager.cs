1ef899c406816e143ef9a84b37de301c173a7ff4
Scott <psmuff5@gmail.com>
2021-07-02 15:26:48 +1000

    Engine and armour hub upgrading
    Added functionality to the engine and armour part of the UI

--- Assets/Scripts/HubManager.cs
+++ Assets/Scripts/HubManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class HubManager : MonoBehaviour
 {
-    public Text scrapTotal, weapon;
+    public Text scrapTotal, weapon, engine, armour;
 
     public string[] weapons;
 
@@ -14,18 +14,59 @@ public class HubManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //for testing
         PlayerPrefs.SetInt("PlayerScrapTotal", 500);
         PlayerPrefs.SetInt("PlayerWeaponUpgrades", 500);
+        PlayerPrefs.SetInt("PlayerEngineUpgrades", 500);
+        PlayerPrefs.SetInt("PlayerArmourUpgrades", 500);
+
         PlayerPrefs.SetInt("PlayerTotalWeapons", 0);
+        PlayerPrefs.SetInt("PlayerEngineLevel", 0);
+        PlayerPrefs.SetInt("PlayerArmourLevel", 0);
 
         UpdateHubStats();
+    }
+
+    public void UpgradeEngine()
+    {
+        if (PlayerPrefs.GetInt("PlayerEngineLevel") < maxEngine)
+        {
+            // enough scrap and upgrade available?
+            if (PlayerPrefs.GetInt("PlayerScrapTotal") >= upgradeCost && PlayerPrefs.GetInt("PlayerEngineUpgrades") >= 1)
+            {
+                PlayerPrefs.SetInt("PlayerEngineLevel", PlayerPrefs.GetInt("PlayerEngineLevel") + 1);
+                PlayerPrefs.SetInt("PlayerScrapTotal", PlayerPrefs.GetInt("PlayerScrapTotal") - upgradeCost);
+            }
+        }
+
+        // update text
+        engine.text = "Engine: " + PlayerPrefs.GetInt("PlayerEngineLevel");
+        scrapTotal.text = "Scrap: " + PlayerPrefs.GetInt("PlayerScrapTotal");
+    }
+
+    public void UpgradeArmour()
+    {
+        if (PlayerPrefs.GetInt("PlayerArmourLevel") < maxEngine)
+        {
+            // enough scrap and upgrade available?
+            if (PlayerPrefs.GetInt("PlayerScrapTotal") >= upgradeCost && PlayerPrefs.GetInt("PlayerArmourUpgrades") >= 1)
+            {
+                PlayerPrefs.SetInt("PlayerArmourLevel", PlayerPrefs.GetInt("PlayerArmourLevel") + 1);
+                PlayerPrefs.SetInt("PlayerScrapTotal", PlayerPrefs.GetInt("PlayerScrapTotal") - upgradeCost);
+            }
+        }
+
+        // update text
+        armour.text = "Armour: " + PlayerPrefs.GetInt("PlayerArmourLevel");
+        scrapTotal.text = "Scrap: " + PlayerPrefs.GetInt("PlayerScrapTotal");
     }
 
     void UpdateHubStats()
     {
         scrapTotal.text = "Scrap: " + PlayerPrefs.GetInt("PlayerScrapTotal");
         weapon.text = "Weapon: " + weapons[PlayerPrefs.GetInt("PlayerCurrentWeapon")];
-
+        engine.text = "Engine: " + PlayerPrefs.GetInt("PlayerEngineLevel");
+        armour.text = "Armour: " + PlayerPrefs.GetInt("PlayerArmourLevel");
     }
 
 
@@ -35,6 +76,7 @@ public class HubManager : MonoBehaviour
         {
             PlayerPrefs.SetInt("PlayerCurrentWeapon", PlayerPrefs.GetInt("PlayerCurrentWeapon") - 1);
         }
+        // update text
         weapon.text = "Weapon: " + weapons[PlayerPrefs.GetInt("PlayerCurrentWeapon")];
     }
 
@@ -46,22 +88,19 @@ public class HubManager : MonoBehaviour
             return;
         }
 
-        //if (PlayerPrefs.GetInt("PlayerCurrentWeapon") < PlayerPrefs.GetInt("PlayerTotalWeapons"))
-        //{
-            // does player already own the next weapon?
-            if (PlayerPrefs.GetInt("PlayerCurrentWeapon")+1 <= PlayerPrefs.GetInt("PlayerTotalWeapons"))
-            {
-                PlayerPrefs.SetInt("PlayerCurrentWeapon", PlayerPrefs.GetInt("PlayerCurrentWeapon") + 1);
-            }
-            // enough scrap & cache available?
-            else if (PlayerPrefs.GetInt("PlayerScrapTotal") >= upgradeCost && PlayerPrefs.GetInt("PlayerWeaponUpgrades") >= 1)
-            {
-                PlayerPrefs.SetInt("PlayerCurrentWeapon", PlayerPrefs.GetInt("PlayerCurrentWeapon") + 1);
-                PlayerPrefs.SetInt("PlayerScrapTotal", PlayerPrefs.GetInt("PlayerScrapTotal") - upgradeCost);
-                PlayerPrefs.SetInt("PlayerTotalWeapons", PlayerPrefs.GetInt("PlayerTotalWeapons") + 1);
-            }
-            
-        //}
+        // does player already own the next weapon?
+        if (PlayerPrefs.GetInt("PlayerCurrentWeapon")+1 <= PlayerPrefs.GetInt("PlayerTotalWeapons"))
+        {
+            PlayerPrefs.SetInt("PlayerCurrentWeapon", PlayerPrefs.GetInt("PlayerCurrentWeapon") + 1);
+        }
+        // enough scrap & cache available?
+        else if (PlayerPrefs.GetInt("PlayerScrapTotal") >= upgradeCost && PlayerPrefs.GetInt("PlayerWeaponUpgrades") >= 1)
+        {
+            PlayerPrefs.SetInt("PlayerCurrentWeapon", PlayerPrefs.GetInt("PlayerCurrentWeapon") + 1);
+            PlayerPrefs.SetInt("PlayerScrapTotal", PlayerPrefs.GetInt("PlayerScrapTotal") - upgradeCost);
+            PlayerPrefs.SetInt("PlayerTotalWeapons", PlayerPrefs.GetInt("PlayerTotalWeapons") + 1);
+        }
+
         // update text
         weapon.text = "Weapon: " + weapons[PlayerPrefs.GetInt("PlayerCurrentWeapon")];
         scrapTotal.text = "Scrap: " + PlayerPrefs.GetInt("PlayerScrapTotal");
