580be4d9a82396f4b98f2de86f7edf9b94fc6523
Scott <psmuff5@gmail.com>
2021-09-16 14:47:20 +1000

    Added health and energy pickup sounds
    The sounds are quite basic. Also noticed a bug; the collector shoots very loudly.

--- Assets/Scripts/ResourcePickup.cs
+++ Assets/Scripts/ResourcePickup.cs
@@ -6,17 +6,30 @@ public class ResourcePickup : Collectible
 {
     public float hpGiven, energyGiven;
 
+    public AudioClip[] healthSounds, energySounds;
+
+    AudioPlayer audioPlayer;
+
+    private void Start()
+    {
+        audioPlayer = GetComponent<AudioPlayer>();
+    }
+
     public override void OnPickup(GameObject player)
     {
         player.GetComponent<PlayerController>().HP += hpGiven;
         if (player.GetComponent<PlayerController>().HP > player.GetComponent<PlayerController>().maxHP)
         {
+            audioPlayer.PlayClipAt(healthSounds, .4f);
             player.GetComponent<PlayerController>().HP = player.GetComponent<PlayerController>().maxHP;
+            
         }
         player.GetComponent<PlayerController>().energy += energyGiven;
         if (player.GetComponent<PlayerController>().energy > player.GetComponent<PlayerController>().maxEnergy)
         {
+            audioPlayer.PlayClipAt(energySounds, .4f);
             player.GetComponent<PlayerController>().energy = player.GetComponent<PlayerController>().maxEnergy;
+            
         }
     }
 }
