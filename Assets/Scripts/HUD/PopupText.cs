c16e21d76444960785c52a6be95a87c95210488e
Scott <psmuff5@gmail.com>
2021-09-23 23:24:23 +1000

    Popup Text Tweak

--- Assets/Scripts/HUD/PopupText.cs
+++ Assets/Scripts/HUD/PopupText.cs
@@ -7,11 +7,19 @@ public class PopupText : MonoBehaviour
 {
     public GameObject popup;
 
-    public float destroyTime = 3f;
+    public float destroyTime = 3f, randomOffsetAmountX=1f, randomOffsetAmountY = .5f;
 
     private void Start()
     {
         Destroy(gameObject, destroyTime);
+
+        // offset the position a random amount so that popups close to each other don't overlap so much
+        float randomX = Random.Range(-randomOffsetAmountX, randomOffsetAmountX);
+        float randomY = Random.Range(-randomOffsetAmountY, randomOffsetAmountY);
+        //Vector3 offset = new Vector3(randomX, randomY, 0);
+
+        transform.position += Camera.main.transform.up * randomY;
+        transform.position += Camera.main.transform.right * randomX;
     }
 
     private void Update()
