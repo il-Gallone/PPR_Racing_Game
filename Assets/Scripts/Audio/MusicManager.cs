734da01ae368b54a315b0c7de9bbfba17f9450ee
Scott <psmuff5@gmail.com>
2021-09-24 13:40:37 +1000

    Menu Audio Fix and Completion

--- Assets/Scripts/Audio/MusicManager.cs
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -8,6 +8,9 @@ public class MusicManager : MonoBehaviour
     public AudioSource audioSource;
     public AudioClip menuMusic, raidMusic;
 
+    int prevScene = 0, currentScene = 0;
+
+
     private void OnEnable()
     {
         SceneManager.sceneLoaded += OnSceneChanged;
@@ -20,11 +23,14 @@ public class MusicManager : MonoBehaviour
 
     void OnSceneChanged(Scene scene, LoadSceneMode mode)
     {
-        if (scene.buildIndex==0)
+        prevScene = currentScene;
+        currentScene = scene.buildIndex;
+
+        if (scene.buildIndex== 0/*main menu*/ && prevScene != 1 /*map scene*/)
         {
             audioSource.clip = menuMusic;
             audioSource.Play();
-        } else if (scene.buildIndex>=4)
+        } else if (scene.buildIndex>=4) // raid levels should be above build index 4. Probably a better way to do this but it works for now
         {
             audioSource.clip = raidMusic;
             audioSource.Play();
