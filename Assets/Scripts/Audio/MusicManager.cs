ae96cb1eb2bc2be9c1ac9388bb0d7160ab55a3b6
Scott <psmuff5@gmail.com>
2021-10-03 17:37:18 +1100

    Audio Sliders Improvement
    Sliders now more accurately represent their volumes. Still need to properly load the saved settings on startup. Not working properly atm

--- Assets/Scripts/Audio/MusicManager.cs
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MusicManager : MonoBehaviour
 {
@@ -15,23 +16,33 @@ public class MusicManager : MonoBehaviour
 
     public AudioMixer masterAudioMixer;
 
+    public Slider master, music, sfx;
+
     private void Awake()
     {
         // set volume sliders
+        //float volume;
+        //masterAudioMixer.GetFloat("MasterVolume", out volume);
+        //master.value = Mathf.Log10(volume) * 20;
 
+        //masterAudioMixer.GetFloat("MusicVolume", out volume);
+        //music.value = Mathf.Log10(volume) * 20;
+
+        //masterAudioMixer.GetFloat("SFXVolume", out volume);
+        //sfx.value = Mathf.Log10(volume) * 20;
     }
 
     public void SetMasterVolume(float volume)
     {
-        masterAudioMixer.SetFloat("MasterVolume", volume);
+        masterAudioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
     }
     public void SetMusicVolume(float volume)
     {
-        masterAudioMixer.SetFloat("MusicVolume", volume);
+        masterAudioMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
     }
     public void SetSFXVolume(float volume)
     {
-        masterAudioMixer.SetFloat("SFXVolume", volume);
+        masterAudioMixer.SetFloat("SFXVolume", Mathf.Log10(volume) * 20);
     }
 
     private void OnEnable()
